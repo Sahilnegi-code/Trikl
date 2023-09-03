1dcf20058f85dca937af3e3ec32c770226f1f404
SahilNegi <sahilnegi.pang@gmail.com>
2023-09-03 11:26:44 +0530

    project has been done.

---
+++ src/DragDrop.js
@@ -0,0 +1,55 @@
+import React, {useState , useEffect} from 'react'
+import Draggable from "react-draggable"
+const DragDrop = () => {
+    const [ val ,setVal ] =    useState("Empty");
+    const [imgstyle , setImgstyle ] = useState("")
+    const call = async()=>{
+        
+     const res = await  fetch("https://api.unsplash.com/photos/random/?client_id=PJ6KrG-bxBXborLQ5z5SAXVV9OmK-uIpeI4uiCxatX8");
+     const data = await res.json();
+setImgstyle(data.urls.regular);
+
+    }
+useEffect( ()=>{
+    call();
+},[]);
+  return (
+    <>
+       <div style= {
+    {
+        backgroundImage:`url(${imgstyle})`,
+            height: '100vh',
+            border: '2px solid red',
+            backgroundSize: '100%',
+            backgroundRepeat: 'no-repeat'
+    }
+    } >
+      <Draggable    
+      axis='both'
+      grid={[100, 20]}
+      handle= ".handle"
+      position={null}
+      scale={1}
+
+      >
+        
+            <div>
+            <textarea className='handle'
+                value={val}
+             
+                onChange={(e) => {
+                    setVal(e.target.value);
+                }}
+    style={{ overflowY:'hidden',color:'white', fontSize:'40px' ,background:'transparent', outline:'none', width:'300px' , height:'100px', borderBottom:'grey'}}
+            />  
+            </div>
+    
+
+   
+        </Draggable>
+        </div>
+    </>
+  )
+}
+
+export default DragDrop
